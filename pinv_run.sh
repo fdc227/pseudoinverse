7ddebb1e941581f6f7bf6bea5cd2f5ff85112940
fdc227 <37053881+fdc227@users.noreply.github.com>
2021-12-30 02:01:20 +0000

    Add files via upload

---
+++ pinv_run.sh
@@ -0,0 +1,3 @@
+g++ -c pinv.cpp  -Wl,--start-group ${MKLROOT}/lib/intel64/libmkl_intel_ilp64.a ${MKLROOT}/lib/intel64/libmkl_intel_thread.a ${MKLROOT}/lib/intel64/libmkl_core.a -Wl,--end-group -liomp5 -lpthread -lm -ldl  -DMKL_ILP64  -m64  -I"${MKLROOT}/include"
+
+g++ pinv.o -o pinv  -DMKL_ILP64  -m64  -I"${MKLROOT}/include"  -Wl,--start-group ${MKLROOT}/lib/intel64/libmkl_intel_ilp64.a ${MKLROOT}/lib/intel64/libmkl_intel_thread.a ${MKLROOT}/lib/intel64/libmkl_core.a -Wl,--end-group -liomp5 -lpthread -lm -ldl
