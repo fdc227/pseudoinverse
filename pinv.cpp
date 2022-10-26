30dff5aa14b655e62792ed624b58195e88e17c8a
fdc227 <37053881+fdc227@users.noreply.github.com>
2022-10-27 00:56:48 +0100

    Add files via upload
    Bug fix in function diagonal_gen

--- pinv.cpp
+++ pinv.cpp
@@ -30,7 +30,7 @@ void mat_ptr(double* A, int m, int n)
 
 void diagonal_gen(double* a, int n, double* c, int k)
 {
-    int lda = n+1;
+    int lda = n;
     for (int i = 0; i < k; i++)
     {
         a[i*lda+i] = c[i];
@@ -72,7 +72,7 @@ void pinv_driver(double* A, int m, int n, int k, double* S, double* Smat, double
     gesvd_driver(A, m, n, S, U, Vt, Superb, info_svd);
     vec_inverse(S, k);
     vec_zero(Smat, m*n);
-    diagonal_gen(Smat, n, S, k);
+    diagonal_gen(Smat, m, S, k);
     vec_zero(USigma, m*n);
     vec_zero(Pinv, m*n);
     cblas_dgemm(CblasRowMajor, CblasTrans, CblasNoTrans, n, m, n, 1.0, Vt, n, Smat, m, 0.0, USigma, m);
@@ -102,30 +102,36 @@ void pinv_API(vector<double>& A_vec, int m, int n, vector<double>& Pinv_vec)
 
 int main(void)
 {
-    int m = 3;
-    int n = 2;
-    int k = MINMN(m, n);
-    double* U, *Vt, *S, *Superb, *Smat, *Sp, *USigma, *Pinv;
-    U = new double[m*m];
-    S = new double[MINMN(m, n)];
-    Vt = new double[n*n];
-    Superb = new double[MINMN(m,n)-1];
-    Smat = new double[m*n];
-    Sp = new double[m*n];
-    USigma = new double[m*n];
-    Pinv = new double[m*n];
-    int info_svd;
+    // int m = 3;
+    // int n = 2;
+    // int k = MINMN(m, n);
+    // double* U, *Vt, *S, *Superb, *Smat, *Sp, *USigma, *Pinv;
+    // U = new double[m*m];
+    // S = new double[MINMN(m, n)];
+    // Vt = new double[n*n];
+    // Superb = new double[MINMN(m,n)-1];
+    // Smat = new double[m*n];
+    // Sp = new double[m*n];
+    // USigma = new double[m*n];
+    // Pinv = new double[m*n];
+    // int info_svd;
 
-    double A[6] = {2, -1, 0, 4, 3, -2};
+    // double A[6] = {2, -1, 0, 4, 3, -2};
 
-    pinv_driver(A, m, n, k, S, Smat, Sp, U, Vt, Superb, &info_svd, USigma, Pinv);
+    // pinv_driver(A, m, n, k, S, Smat, Sp, U, Vt, Superb, &info_svd, USigma, Pinv);
 
-    mat_ptr(Pinv, n, m);
+    // mat_ptr(Pinv, n, m);
 
-    vector<double> A_vec {2, -1, 0, 4, 3, -2};
-    vector<double> Pinv_vec(6);
+    // vector<double> A_vec {2, -1, 0, 4, 3, -2};
+    // vector<double> Pinv_vec(6);
 
-    pinv_API(A_vec, 3, 2, Pinv_vec);
-    mat_ptr(&*Pinv_vec.begin(), 2, 3);
+    // pinv_API(A_vec, 3, 2, Pinv_vec);
+    // mat_ptr(&*Pinv_vec.begin(), 2, 3);
+
+    vector<double> B {1,2,3,4,5,6,7,8};
+    vector<double> Pinv(8);
+
+    pinv_API(B, 2, 4, Pinv);
+    mat_ptr(&*Pinv.begin(), 4, 2);
 
 }
